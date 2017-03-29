45556f935a3da6eb03d2a05200c58fcc074ae9d4
isabellarner <larner.isabel@gmail.com>
2017-03-29 20:51:04 +0100

    Interactive student list creation

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-# putting the students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Kreuger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-# cohort is saved as a symbol as will be immutable
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -29,6 +13,26 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students."
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get first name
+  name = gets.chomp
+  # while name is not empty, repeat this code
+  while !name.empty? do
+    # add student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
