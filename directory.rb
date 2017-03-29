51a3ec88f1bc7c922942923897f45b4dec168c61
isabellarner <larner.isabel@gmail.com>
2017-03-29 17:52:42 +0100

    List with comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freddie Kreuger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# we print the total number of students
 print "Overall, we have "
+# print() will not add a new line character
 print 11
 puts " great students"
