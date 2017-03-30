c9ea3bf62d91a24335d3ba1dac312f35f6f3cbf3
isabellarner <larner.isabel@gmail.com>
2017-03-30 14:58:35 +0100

    Ex 2

--- directory.rb
+++ directory.rb
@@ -4,9 +4,20 @@ def print_header
 end
 
 def print(students)
-students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+  if student[:name].start_with?("B")
+  x = "#{index}. #{student[:name]} (#{student[:cohort]} cohort). Hobbies: #{student[:hobby]}. Height: #{student[:height]}."
+  puts x.center(100)
+   end
  end
+end
+
+def loop(students)
+  x = 0
+  until x == students.count
+    puts "#{students[x][:name]} (#{students[x][:cohort]} cohort). Hobbies: #{students[x][:hobby]}. Height: #{students[x][:height]}."
+  x += 1
+end
 end
 
 def print_footer(students)
@@ -14,21 +25,32 @@ def print_footer(students)
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students, then their personal information."
+  puts "To finish, just leave each question blank (press enter)."
   # create an empty array
   students = []
   # get first name
   name = gets.chomp
+  puts "And their hobby?"
+  hobby = gets.chomp
+  puts "And their height?"
+  height = gets.chomp
+  puts "Their cohort?"
+  cohort = gets.chomp.to_sym
   # while name is not empty, repeat this code
   while !name.empty? do
     # add student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    students << {name: name, cohort: cohort, hobby: hobby, height: height}
+    puts "Now we have #{students.count} students. Please enter another."
+    # get more info from the user
     name = gets.chomp
+    puts "And their hobby?"
+    hobby = gets.chomp
+    puts "And their height?"
+    height = gets.chomp
+    puts "Their cohort?"
+    cohort = gets.chomp.to_sym
   end
-  #return the array of students
   students
 end
 
@@ -36,3 +58,4 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
+loop(students)
