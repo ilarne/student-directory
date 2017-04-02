366c46bc793b6a88dcad1c1dda5466a18c23f40e
isabellarner <larner.isabel@gmail.com>
2017-04-02 13:38:28 +0100

    Saving files + .gitignore

--- directory.rb
+++ directory.rb
@@ -1,8 +1,9 @@
-@students = []
+@students = [] # empty array accessible to all methods
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save list to students.csv"
   puts "9. Exit"
 end
 
@@ -12,6 +13,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -36,21 +39,10 @@ def input_students
   puts "Please enter the names of the students, then their personal information."
   puts "To finish,  press Enter."
   name = gets.chomp
-  puts "And their hobby?"
-  hobby = gets.chomp
-  puts "And their height?"
-  height = gets.chomp
-  puts "Their cohort?"
-  cohort = gets.chomp
-
   while !name.empty? do
     # add student hash to the array
     @students << {
-      name: default(name),
-      cohort: default_cohort(cohort).to_sym.capitalize,
-      hobby: default(hobby),
-      height: default(height)
-  }
+      name: name, cohort: :november }
     if @students.count == 1
       puts "Now we have 1 student. Please enter another."
     else
@@ -58,22 +50,6 @@ def input_students
   end
     # get more info from the user
     name = gets.chomp
-    if !name.empty?
-    puts "And their hobby?"
-    hobby = gets.chomp
-    puts "And their height?"
-    height = gets.chomp
-    puts "Their cohort?"
-    cohort = gets.chomp
-   end
-  end
-end
-
-def default(x)
-  if !x.empty?
-    x
-  else
-    "Unknown"
   end
 end
 
@@ -84,21 +60,12 @@ def print_header
    puts y.center(100)
 end
 
-def default_cohort(x)
-  month = %w(january february march april may june july august september october november december)
-  until month.include?(x.downcase)
-    puts "Please choose a correct month."
-    x = gets.chomp
-  end
-  x
-end
-
 def print_students_list
  if @students.count == 0
    puts "There are no students enrolled..."
  else
-  @students.each_with_index do |student, index|
-  x = "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort). Hobbies: #{student[:hobby]}. Height: #{student[:height]}."
+  @students.each do |student|
+  x = "#{student[:name]} (#{student[:cohort]} cohort)."
   puts x.center(100)
    end
  end
@@ -110,6 +77,18 @@ def print_footer
   else
    puts "Overall, we have #{@students.count} great @students."
  end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w") # created file object called students.csv with write permissions
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",") # joins array together as a string separated by ","
+    file.puts csv_line  # call puts on file to write csv_line to file
+  end
+  file.close
 end
 
 interactive_menu
